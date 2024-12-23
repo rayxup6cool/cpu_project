31a5c5caa33f8a4f6487d6db17ca036056d35cfd
Ray <rayxup6cool@gmail.com>
2024-12-23 16:40:01 +0800

    first

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 int main()
 {
-    std::cout << "hello world";
+    std::ifstream DRAM_inst_File("DRAM_inst.dat");
+    if (!DRAM_inst_File)
+    {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+    string line;
+    while (getline(DRAM_inst_File, line))
+    {
+        cout << line << endl;
+    }
+    DRAM_inst_File.close();
+    return 0;
 }
